10320705ad6a40a073470b2a3d5d517eaf691ec4
DY <dy@dAir.local>
2019-08-24 19:02:12 +0530

    add new solution

---
+++ topics/heap/create-stack-using-heap/index.js
@@ -0,0 +1,26 @@
+const { Heap } = require('data-structures-again')
+
+class HeapStack {
+    constructor() {
+        this.heap = new Heap((a, b) => {
+            return b[0] - a[0]
+        })
+        this.counter = 0
+    }
+
+    push(data) {
+        this.heap.push([this.counter++, data])
+    }
+
+    pop() {
+        const data = this.heap.pop()
+        return data && data[1]
+    }
+
+    peek() {
+        const data = this.heap.peek()
+        return data && data[1]
+    }
+}
+
+module.exports = HeapStack
---
+++ topics/heap/create-stack-using-heap/index.test.js
@@ -0,0 +1,22 @@
+const HeapStack = require('.')
+
+test('push and pop operation', () => {
+    const stack = new HeapStack()
+    expect(stack.pop()).toEqual(undefined)
+    stack.push(1)
+    
+    expect(stack.pop()).toEqual(1)
+
+    stack.push(1)
+    stack.push(2)
+
+    expect(stack.pop()).toEqual(2)
+})
+
+test('peek operation', () => {
+    const stack = new HeapStack()
+    stack.push(1)
+    stack.push(2)
+
+    expect(stack.peek()).toEqual(2)
+})
