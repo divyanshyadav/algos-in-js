e601512f6badb3de153ac47bc4b1a2a86c936c36
dy <divyansh.yadav@gmail.com>
2020-06-16 17:34:42 +0530

    add new dp problem

--- src/topics/dynamic-programing/maximum-subarray-sum/index.js
+++ src/topics/dynamic-programing/maximum-subarray-sum/index.js
@@ -2,27 +2,81 @@
     Time complexity: O(n^3)
     Space complexity: O(1)
 
-    naive solution
+    eg. array = [1, 2, 3]
+    then it will iterate sub-arrays like
+
+    1,
+    2,
+    3,
+
+    1, 2
+    2, 3
+
+    1, 2, 3
 */
 
-function maxSubarraySum (array) {
-    let max = -Infinity
+function maxSubarraySumO3 (array) {
+    const sum = (array, start, end) => {
+        let total = 0
+        for (let i = start; i < end; i++) total += array[i]
+        return total
+    }
 
+    let maxSubarraySum = -Infinity
+
+    // sub arrays
     for (let size = 1; size <= array.length; size++) {
-        for (let start = 0; start <= array.length - size; start += 1) {
-            let subArraySum = 0
-
-            for (let index = start; index < start + size; index++) {
-                subArraySum += array[index]
+        for (let start = 0; start < array.length; start += 1) {
+            // invalid subarray check
+            if (start + size >= array.length) {
+                break
             }
 
-            if (subArraySum > max) {
-                max = subArraySum
+            const subArraySum = sum(array, start, start + size)
+
+            maxSubarraySum = Math.max(subArraySum, maxSubarraySum)
+        }
+    }
+
+    return maxSubarraySum
+}
+
+/*
+    Time complexity: O(n^2)
+    Space complexity: O(1)
+
+    eg. array = [1, 2, 3]
+    then it will iterate sub-arrays like
+
+    1,
+    1, 2
+    1, 2, 3
+
+    2
+    2, 3
+
+    3
+*/
+
+function maxSubarraySumO2 (array) {
+    let max = -Infinity
+    for (let start = 0; start < array.length; start++) {
+        let sum = 0
+        // sub arrays
+        for (let size = 1; size < array.length; size++) {
+            // invalid subarray check
+            if (start + size >= array.length) {
+                break
             }
+
+            // adding last element in sub-array
+            sum += array[start + size - 1]
+            max = Math.max(sum, max)
         }
     }
 
     return max
 }
 
-module.exports = maxSubarraySum
+module.exports = maxSubarraySumO3
+module.exports = maxSubarraySumO2
--- src/topics/dynamic-programing/maximum-subarray-sum/index.test.js
+++ src/topics/dynamic-programing/maximum-subarray-sum/index.test.js
@@ -4,3 +4,8 @@ test('case 1', () => {
     const array = [-2, -3, 4, -1, -2, 1, 5, -3]
     expect(maxSubarraySum(array)).toBe(7)
 })
+
+test('case 2', () => {
+    const array = [3, -2, 5, -1]
+    expect(maxSubarraySum(array)).toBe(6)
+})
