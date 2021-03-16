804108cc38e81f72cc42370b5d0ff11f611450a9
Divyansh <divyansh.yadav@gmail.com>
2021-03-16 22:48:52 +0530

    add recursive insertion sort

--- src/topics/sorting/insertion-sort/index.js
+++ src/topics/sorting/insertion-sort/index.js
@@ -14,4 +14,33 @@ function sortDesc (array) {
     return array
 }
 
-module.exports = sortDesc
+function sortAcc (array) {
+    insertionSortRec(array, 0)
+    return array
+}
+
+function insertionSortRec (array, index) {
+    if (index >= array.length) {
+        return
+    }
+
+    insert(array, index)
+    insertionSortRec(array, index + 1)
+}
+
+function insert (array, index) {
+    const num = array[index]
+    let j = index - 1
+
+    while (array[j] > num) {
+        array[j + 1] = array[j]
+        j--
+    }
+
+    array[j + 1] = num
+}
+
+module.exports = {
+    sortDesc,
+    sortAcc
+}
--- src/topics/sorting/insertion-sort/index.test.js
+++ src/topics/sorting/insertion-sort/index.test.js
@@ -1,7 +1,13 @@
-const sortDesc = require('.')
+const { sortAcc, sortDesc } = require('.')
 
 test('case 1: sort desc', () => {
     const input = [31, 41, 59, 26, 41, 58]
     const output = [59, 58, 41, 41, 31, 26]
     expect(sortDesc(input)).toEqual(output)
+})
+
+test('case 2: sort acc', () => {
+    const input = [5, 4, 3, 2, 1]
+    const output = [1, 2, 3, 4, 5]
+    expect(sortAcc(input)).toEqual(output)
 })
