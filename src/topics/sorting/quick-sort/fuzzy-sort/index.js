274867a8ffdb5609aea84d4850de0e56614e103e
Divyansh <divyansh.yadav@gmail.com>
2021-04-04 10:15:47 +0530

    add fuzzy sort algo

--- src/topics/sorting/quick-sort/fuzzy-sort/index.js
+++ src/topics/sorting/quick-sort/fuzzy-sort/index.js
@@ -1,3 +1,9 @@
+/*
+    Reference: https://alumni.media.mit.edu/~dlanman/courses/cs157/HW3.pdf
+*/
+
+const { swap } = require('../../../../utils')
+
 function findIntersection (intervals, start, end) {
     let [a, b] = intervals[end]
 
@@ -13,6 +19,51 @@ function findIntersection (intervals, start, end) {
     return [a, b]
 }
 
+function fuzzySort (intervals) {
+    function helper (intervals, start, end) {
+        if (start >= end) {
+            return
+        }
+
+        const [a, b] = findIntersection(intervals, start, end)
+        const midEnd = partitionRight(intervals, start, end, a)
+        const midStart = partitionLeftMid(intervals, start, midEnd, b)
+        helper(intervals, start, midStart - 1)
+        helper(intervals, midEnd + 1, end)
+    }
+
+    helper(intervals, 0, intervals.length - 1)
+}
+
+function partitionRight (intervals, start, end, value) {
+    let i = start - 1
+    for (let j = 0; j < end; j++) {
+        if (intervals[j][0] <= value) {
+            swap(intervals, ++i, j)
+        }
+    }
+
+    swap(intervals, ++i, end)
+
+    return i
+}
+
+function partitionLeftMid (intervals, start, end, value) {
+    let i = start - 1
+    for (let j = 0; j < end; j++) {
+        if (intervals[j][1] < value) {
+            swap(intervals, ++i, j)
+        }
+    }
+
+    swap(intervals, ++i, end)
+
+    return i
+}
+
 module.exports = {
-    findIntersection
+    findIntersection,
+    partitionRight,
+    partitionLeftMid,
+    fuzzySort
 }
--- src/topics/sorting/quick-sort/fuzzy-sort/index.test.js
+++ src/topics/sorting/quick-sort/fuzzy-sort/index.test.js
@@ -1,4 +1,4 @@
-const { findIntersection } = require('.')
+const { findIntersection, partitionRight, partitionLeftMid, fuzzySort } = require('.')
 
 test('findIntersection | case 1', () => {
     const intervals = [
@@ -19,4 +19,33 @@ test('findIntersection | case 2', () => {
     const [start, end] = findIntersection(intervals, 0, intervals.length - 1)
     expect(start).toBe(3)
     expect(end).toBe(4)
+})
+
+test('partitionRight | case 1', () => {
+    const intervals = [
+        [8, 9],
+        [3, 5], [4, 7],
+        [1, 2]
+    ]
+
+    const index = partitionRight(intervals, 0, intervals.length - 1, 4)
+    expect(index).toBe(2)
+})
+
+test('partitionLeftMid | case 1', () => {
+    const intervals = [[3, 5], [4, 7], [1, 2], [8, 9]]
+
+    const index = partitionLeftMid(intervals, 0, 2, 5)
+    expect(index).toBe(0)
+})
+
+test('fuzzy sort', () => {
+    const intervals = [
+        [8, 9],
+        [3, 5], [1, 2],
+        [4, 7]
+    ]
+
+    fuzzySort(intervals)
+    expect(intervals).toEqual([[1, 2], [4, 7], [3, 5], [8, 9]])
 })
