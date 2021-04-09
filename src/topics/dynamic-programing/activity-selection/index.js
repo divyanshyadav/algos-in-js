a6fa97c6a0a04af2e7b91121006841d2b8dc991c
Divyansh <divyansh.yadav@gmail.com>
2021-04-09 10:24:39 +0530

    add readme to activity-selection

--- src/topics/dynamic-programing/activity-selection/index.js
+++ src/topics/dynamic-programing/activity-selection/index.js
@@ -1,10 +1,15 @@
 function activitySelection (activities) {
     activities.unshift([0, 0])
     activities.push([+Infinity, +Infinity])
-    return helper(activities, 0, activities.length - 1)
+    const maxCompatibilityActivities = new Array(activities.length).fill(0)
+    helper(activities, 0, activities.length - 1, maxCompatibilityActivities)
+    maxCompatibilityActivities.shift()
+    maxCompatibilityActivities.pop()
+    console.log(maxCompatibilityActivities)
+    return maxCompatibilityActivities
 }
 
-function helper (activities, low, high) {
+function helper (activities, low, high, maxCompatibilityActivities) {
     if (low > high) {
         return 0
     }
@@ -12,17 +17,64 @@ function helper (activities, low, high) {
     const startTime = activities[low][1]
     const finishTime = activities[high][0]
     let max = 0
+    let compatibleActivity = -1
+    let prevCompatibleActivity = -1
     for (let i = low + 1; i < high; i++) {
         let ans = 0
         if (activities[i][0] >= startTime && activities[i][1] <= finishTime) {
-            ans = 1 + helper(activities, low, i) + helper(activities, i, high)
+            ans = 1 + helper(activities, low, i, maxCompatibilityActivities) + helper(activities, i, high, maxCompatibilityActivities)
+            prevCompatibleActivity = compatibleActivity
+            compatibleActivity = i
         }
-        max = Math.max(max, ans)
+
+        if (ans > max) {
+            max = Math.max(max, ans)
+            if (prevCompatibleActivity !== -1) {
+                maxCompatibilityActivities[prevCompatibleActivity] = 0
+            }
+            maxCompatibilityActivities[compatibleActivity] = 1
+        }
     }
 
     return max
 }
 
+function activitySelectionUsingDP (activities) {
+    activities.unshift([0, 0])
+    activities.push([+Infinity, +Infinity])
+    const dp = new Array(activities.length)
+        .fill().map(_ => new Array(activities.length).fill(0))
+
+    for (let k = 2
+        ; k < activities.length; k++) {
+        for (let low = 0, high = k; high < activities.length; low++, high++) {
+            const startTime = activities[low][1]
+            const finishTime = activities[high][0]
+
+            let max = 0
+            for (let i = low + 1; i < high; i++) {
+                let ans = 0
+                if (activities[i][0] >= startTime && activities[i][1] <= finishTime) {
+                    ans = 1
+                    if (low <= i - 1) {
+                        ans += dp[low + 1][i - 1]
+                    }
+
+                    if (i + 1 <= high) {
+                        ans += dp[i + 1][high - 1]
+                    }
+                }
+                max = Math.max(max, ans)
+            }
+
+            dp[low][high] = max
+        }
+    }
+
+    console.log(dp)
+}
+
 module.exports = {
-    activitySelection
+    activitySelection,
+    activitySelectionUsingDP
 }
--- src/topics/dynamic-programing/activity-selection/index.test.js
+++ src/topics/dynamic-programing/activity-selection/index.test.js
@@ -1,6 +1,6 @@
-const { activitySelection } = require('.')
+const { activitySelection, activitySelectionUsingDP } = require('.')
 
-test('case 1', () => {
+test.only('case 1', () => {
     const activities = [
         [3, 6], [7, 12],
         [1, 8], [9, 10],
@@ -8,5 +8,29 @@ test('case 1', () => {
         [4, 13]
     ]
 
-    expect(activitySelection(activities)).toBe(3)
+    expect(activitySelection(activities)).toEqual([1, 0, 0, 1, 1, 0, 0])
+})
+
+test.only('case 2', () => {
+    const activities = [
+        [1, 4], [3, 5],
+        [0, 6], [5, 7],
+        [3, 9], [5, 9],
+        [6, 10], [8, 11],
+        [8, 12], [2, 14],
+        [12, 16]
+    ]
+
+    expect(activitySelection(activities)).toEqual([1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1])
+})
+
+test('activitySelectionUsingDP | case 1', () => {
+    const activities = [
+        [3, 6], [7, 12],
+        [1, 8], [9, 10],
+        [11, 15], [2, 5],
+        [4, 13]
+    ]
+
+    expect(activitySelectionUsingDP(activities)).toBe(3)
 })
