ea22cfbd62155432291f238492e20a012aa0a9fd
Divyansh <divyansh.yadav@gmail.com>
2021-05-10 16:28:51 +0530

    add 0 1 knapsack

--- src/topics/dynamic-programing/01-knapsack/index.js
+++ src/topics/dynamic-programing/01-knapsack/index.js
@@ -1,6 +1,8 @@
-function calculate (weights, costs, capacity) {
+function zeroOneKnapsack (weights, profits, capacity) {
+    const result = new Array(weights.length).fill(0)
+
     function helper (index, weight) {
-        if (index >= costs.length || weight <= 0) {
+        if (index >= profits.length || weight <= 0) {
             return 0
         }
 
@@ -8,13 +10,59 @@ function calculate (weights, costs, capacity) {
             return helper(index + 1, weight)
         }
 
-        return Math.max(
-            helper(index + 1, weight),
-            costs[index] + helper(index + 1, weight - weights[index])
-        )
+        const take = profits[index] + helper(index + 1, weight - weights[index])
+        const dontTake = helper(index + 1, weight)
+
+        if (take > dontTake) {
+            result[index] = 1
+        } else {
+            result[index] = 0
+        }
+
+        return Math.max(take, dontTake)
     }
 
-    return helper(0, capacity)
+    helper(0, capacity)
+
+    return result.reduce((acc, item, index) => {
+        if (item === 1) {
+            acc.push(index)
+        }
+
+        return acc
+    }, [])
 }
 
-module.exports = calculate
+function zeroOneKnapsackUsingDP (weights, profits, capacity) {
+    const dp = new Array(weights.length + 1).fill(0).map(_ => new Array(capacity + 1).fill(0))
+
+    for (let i = 1; i <= weights.length; i++) {
+        for (let j = 0; j <= capacity; j++) {
+            if (j - weights[i - 1] >= 0) {
+                dp[i][j] = profits[i - 1] + dp[i - 1][j - weights[i - 1]]
+            }
+
+            dp[i][j] = Math.max(dp[i][j], dp[i - 1][j])
+        }
+    }
+
+    const result = []
+    let i = weights.length
+    let j = capacity
+
+    while (i > 0) {
+        if (dp[i - 1][j] !== dp[i][j]) {
+            result.push(i - 1)
+            j = j - weights[i - 1]
+        }
+
+        i--
+    }
+
+    return result.reverse()
+}
+
+module.exports = {
+    zeroOneKnapsack,
+    zeroOneKnapsackUsingDP
+}
--- src/topics/dynamic-programing/01-knapsack/index.test.js
+++ src/topics/dynamic-programing/01-knapsack/index.test.js
@@ -1,9 +1,37 @@
-const calculate = require('.')
+const { zeroOneKnapsack, zeroOneKnapsackUsingDP } = require('.')
 
-test('case 1', () => {
-    const weights = [10, 20, 30]
-    const costs = [60, 100, 120]
-    const capacity = 50
+describe('1 0 Knapsack using Recursion', () => {
+    test('case 1', () => {
+        const weights = [10, 20, 30]
+        const profits = [60, 100, 120]
+        const capacity = 50
 
-    expect(calculate(weights, costs, capacity)).toBe(220)
+        expect(zeroOneKnapsack(weights, profits, capacity)).toEqual([1, 2])
+    })
+
+    test('case 2', () => {
+        const weights = [10, 20, 30, 10]
+        const profits = [60, 100, 120, 130]
+        const capacity = 50
+
+        expect(zeroOneKnapsack(weights, profits, capacity)).toEqual([0, 2, 3])
+    })
+})
+
+describe('1 0 Knapsack using DP', () => {
+    test('case 1', () => {
+        const weights = [10, 20, 30]
+        const profits = [60, 100, 120]
+        const capacity = 50
+
+        expect(zeroOneKnapsackUsingDP(weights, profits, capacity)).toEqual([1, 2])
+    })
+
+    test('case 2', () => {
+        const weights = [10, 20, 30, 10]
+        const profits = [60, 100, 120, 130]
+        const capacity = 50
+
+        expect(zeroOneKnapsackUsingDP(weights, profits, capacity)).toEqual([0, 2, 3])
+    })
 })
