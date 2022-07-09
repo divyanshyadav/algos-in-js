f3b45d14cffb078270c814cab8ad5d05a04d11ee
Divyansh <divyansh.yadav@gmail.com>
2022-07-09 14:23:52 +0100

    add more info about binary search and ternary search

---
+++ src/benchmark/binary-search-vs-ternary-search/README.md
@@ -0,0 +1,5 @@
+![alt text](bs-vs-ts.PNG)
+
+# References
+
+- https://cs.stackexchange.com/questions/29755/why-is-binary-search-faster-than-ternary-search?newreg=ac8d0d3b261e43429e00378cef29abd0
---
+++ src/benchmark/binary-search-vs-ternary-search/index.js
@@ -0,0 +1,9 @@
+const binarySearch = require('../../../topics/searching/binary-search')
+const ternarySearch = require('../../../topics/searching/ternary-search')
+const { benchmark } = require('../../../utils/benchmarkingUtils')
+
+const N = 10000000
+const array = new Array(N + 1).fill(null).map((_, index) => index)
+
+benchmark(binarySearch, array, N)
+benchmark(ternarySearch, array, N)
